349a128f0502f45de1cb15bf6e0add37a1a3ae56
sveta1976z <belous0308212@gmail.com>
2024-02-07 20:22:36 +0300

    55555

--- jg.py
+++ jg.py
@@ -3,7 +3,7 @@ from PIL import Image
 sample=Image.open("3fd84abbd1bc42b3b1534d52e1338bf3.jpeg")
 sample.save('3fd84abbd1bc42b3b1534d52e1338bf3.jpeg')
 sample.show()
-cord = (10, 10, 640, 340) # лево, верх, право, низ
+cord = (10, 10, 640, 340) # лево, верх, право, низ б
 new_picture = picture.crop(cord)
 new_picture.show()
 
