f110291603787630670d79bd71ee79ea6dd20e49
sveta1976z <belous0308212@gmail.com>
2024-02-07 20:20:11 +0300

    44444

--- jg.py
+++ jg.py
@@ -1,10 +1,9 @@
-import sqlite3
+from PIL import Image
 
+sample=Image.open("3fd84abbd1bc42b3b1534d52e1338bf3.jpeg")
+sample.save('3fd84abbd1bc42b3b1534d52e1338bf3.jpeg')
+sample.show()
+cord = (10, 10, 640, 340) # лево, верх, право, низ
+new_picture = picture.crop(cord)
+new_picture.show()
 
-con = sqlite3.connect('rrrrrr.db')
-cursor = con.cursor()
-
-cursor.execute("""CREATE TABLE sveta(namber INTENGEN, name text ,data text);""")
-
-con.close()
-git.init
